a2c120e926203f32377f9181a8618024a79c1ef3
Mocki <605095234@qq.com>
2018-11-10 11:51:53 +0800

    Add HTML file for my blog site

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6*#s1)6$03afo@r1f=fcbqy=+2c$lmstf)t4szxm5%g)85fk8z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
