849029dd5a8ca0f07df195c98f49cd70068986c9
Mocki <605095234@qq.com>
2018-11-10 11:03:29 +0800

    Mocki's Blog is OPEN,first commit

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mocki/projects/python_work/webprojects/mockiblog/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
