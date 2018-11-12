9f8d47df67817cb303c0e23b88491026c15a0502
Mocki <605095234@qq.com>
2018-11-12 20:03:24 +0800

    Added views to create/edit blog post inside the site.

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
-    authhor =  models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    author =  models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
